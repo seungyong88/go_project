4be14dc1a75a6984d75c72160416779b39692471
seungyong88 <gosng2001@naver.com>
2020-06-18 20:52:43 +0900

    struct 사용법

---
+++ section_4/struct_interface.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	var a float64 = x2-x1
+	var b float64 = y2-y1
+	return math.Sqrt(a*a + b*b)
+}
+
+func rectangleArea(x1, y1, x2, y2 float64) float64 {
+	l := distance(x1, y1, x1, y2)
+	w := distance(x1, y1, x2, y1)
+	return l * w
+}
+
+func circleArea(x, y, r float64) float64 {
+	return math.Pi * r * r
+}
+
+type Circle struct {
+	x, y, r float64
+}
+
+func main() {
+	c := Circle{0, 0, 5}
+	var rx1, ry1 float64 = 0, 0
+	var rx2, ry2 float64 = 10, 10
+
+	fmt.Println(rectangleArea(rx1, ry1, rx2, ry2))
+	fmt.Println(circleArea(c.x, c.y, c.r))
+}
