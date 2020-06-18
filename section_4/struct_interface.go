0cd6f3c45912413d95412dae951d4ba35048a37c
seungyong88 <gosng2001@naver.com>
2020-06-18 20:56:33 +0900

    구조체(객체) 업데이트

--- section_4/struct_interface.go
+++ section_4/struct_interface.go
@@ -6,30 +6,45 @@ import (
 )
 
 func distance(x1, y1, x2, y2 float64) float64 {
-	var a float64 = x2-x1
-	var b float64 = y2-y1
+	var a float64 = x2 - x1
+	var b float64 = y2 - y1
 	return math.Sqrt(a*a + b*b)
 }
 
-func rectangleArea(x1, y1, x2, y2 float64) float64 {
-	l := distance(x1, y1, x1, y2)
-	w := distance(x1, y1, x2, y1)
-	return l * w
-}
+// func rectangleArea(x1, y1, x2, y2 float64) float64 {
+// 	l := distance(x1, y1, x1, y2)
+// 	w := distance(x1, y1, x2, y1)
+// 	return l * w
+// }
 
-func circleArea(x, y, r float64) float64 {
-	return math.Pi * r * r
+// func circleArea(x, y, r float64) float64 {
+// 	return math.Pi * r * r
+// }
+
+func (c *Circle) area() float64 {
+	return math.Pi * c.r * c.r
 }
 
 type Circle struct {
 	x, y, r float64
 }
 
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+func (r *Rectangle) area() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+}
+
 func main() {
 	c := Circle{0, 0, 5}
-	var rx1, ry1 float64 = 0, 0
-	var rx2, ry2 float64 = 10, 10
+	r := Rectangle{0, 0, 10, 10}
 
-	fmt.Println(rectangleArea(rx1, ry1, rx2, ry2))
-	fmt.Println(circleArea(c.x, c.y, c.r))
+	fmt.Println(r.area())
+	fmt.Println(c.area())
+
+	// fmt.Println(rectangleArea(rx1, ry1, rx2, ry2))
 }
